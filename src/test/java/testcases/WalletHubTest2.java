fa85c0dde88d5935c0e823bff680190b426bb0b1
c5242 <mashwini010@gmail.com>
2019-08-20 13:03:34 +0530

    added wallethub test 2

---
+++ src/test/java/testcases/WalletHubTest2.java
@@ -0,0 +1,90 @@
+package testcases;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Method;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import pageobject.LoginPage;
+import pageobject.WalletHubHomePage;
+import pageobject.WalletHubProfilePage;
+import util.WTestBase;
+
+public class WalletHubTest2 extends WTestBase{
+
+	public LoginPage login;
+	public WalletHubHomePage homePage;
+	public WalletHubProfilePage profilePage;
+	String reviewString;
+	String TestCaseName;
+	
+	
+	
+	@BeforeClass
+	public void configuration() throws FileNotFoundException
+	{
+		init();
+		initDriver();
+		driverSpecificOperations();
+	}
+	@BeforeMethod
+	public void testbefore(Method method)
+	{
+		String tName = method.getName();
+		String tClass = method.getDeclaringClass().getSimpleName();
+		TestCaseName= tName + "-" + tClass;
+		System.out.println(TestCaseName+" started...");
+	}
+	
+	@AfterMethod ()
+	public void ScreenShot(ITestResult it)
+	{
+		//we can have screenshot code for failed test cases
+		System.out.println(TestCaseName+" Finished ");
+	}
+	
+	@AfterClass
+	public void driverClean() throws InterruptedException
+	{
+		Thread.sleep(10000);
+		driver.quit();
+	}
+		@Test(priority=0)
+		public void login() throws FileNotFoundException
+		{
+			login=PageFactory.initElements(driver, LoginPage.class);
+			homePage=login.loginWalletHub(prop.getProperty("username"),prop.getProperty("password"));			
+		}
+		
+		@Test(priority=1)
+		public void hoverOverStars_Verify() throws FileNotFoundException
+		{
+			homePage.hoverOverStarAndValidate();
+			homePage.hoverOver5Star();
+			
+		}
+		//@Test(priority=2)
+		public void writeReviewAndSubmit() throws FileNotFoundException
+		{
+			homePage.selectReviewCategory(prop.getProperty("reviewCategory"));
+			reviewString=homePage.writeReviewAndSubmit();
+			
+		}
+		
+		//@Test(priority=3)
+		public void gotoProfileAndVerifyReview() throws FileNotFoundException
+		{
+			profilePage=homePage.gotoProfile();
+			profilePage.verifyReviewDisplayed(profilePage.listOfFeeds,reviewString);
+			profilePage.clickOnTabsOnProfilePage(prop.getProperty("walletHubUsername"));
+			profilePage.verifyReviewDisplayed(profilePage.listOfReviews,reviewString);
+			
+		}
+
+}
