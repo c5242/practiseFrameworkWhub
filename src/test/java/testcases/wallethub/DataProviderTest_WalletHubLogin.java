7392260937ac3b52a3e9711692546a4653367f4f
c5242 <mashwini010@gmail.com>
2019-09-03 00:58:03 +0530

    dataprovider single

---
+++ src/test/java/testcases/wallethub/DataProviderTest_WalletHubLogin.java
@@ -0,0 +1,68 @@
+package testcases.wallethub;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Method;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import util.Dataprovider;
+import util.WTestBase;
+import walletHubPageObjects.LoginPage;
+import walletHubPageObjects.WalletHubHomePage;
+import walletHubPageObjects.WalletHubProfilePage;
+
+public class DataProviderTest_WalletHubLogin extends WTestBase{
+
+	public LoginPage login;
+	public WalletHubHomePage homePage;
+	public WalletHubProfilePage profilePage;
+	String reviewString;
+	String TestCaseName;
+	
+	
+	
+	@BeforeTest
+	public void configuration() throws FileNotFoundException
+	{
+		readProperties();
+		getDriver();
+		driverSpecificOperations();
+	}
+	@BeforeMethod
+	public void testbefore(Method method)
+	{
+		String tName = method.getName();
+		String tClass = method.getDeclaringClass().getSimpleName();
+		TestCaseName= tName + "-" + tClass;
+		System.out.println(TestCaseName+" started...");
+	}
+	
+	@AfterMethod ()
+	public void ScreenShot(ITestResult it)
+	{
+		//we can have screenshot code for failed test cases
+		System.out.println(TestCaseName+" Finished ");
+	}
+	
+	@AfterClass
+	public void driverClean() throws InterruptedException
+	{
+		Thread.sleep(2000);
+		driver.quit();
+	}
+
+		@Test(priority=0,dataProvider="Usernamedata",dataProviderClass=Dataprovider.class)
+		public void login(String user,String pass) throws FileNotFoundException
+		{
+			login=PageFactory.initElements(driver, LoginPage.class);
+			homePage=login.loginWalletHub(user,pass);			
+		}
+}
